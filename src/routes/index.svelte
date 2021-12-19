84a7c9c07184c852e36542a982ed6b96c39130ef
EraserHatchling <ismadaniel23sept@gmail.com>
2021-12-19 03:52:47 +0000

    commit3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,21 @@
 <script>
     let userName = "";
     let userAge = "";
-async function getWeather() {
+async function getAge() {
   fetch('https://api.agify.io/?name=' + userName)
 	.then((response) => response.json())
 	.then((data) => {
-  	userName = data.name; // Path for temperature
-  	userAge = data.age; // Path for description
+  	userName = data.name; 
+  	userAge = data.age; 
 	});
 }
 </script>
+
+<h1>Guess Age App</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name:</label>
+    <input id="userName" bind:value={userName} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Age:{userAge}</p>
