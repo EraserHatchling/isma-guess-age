e5406c04f019898205f3f4e450ec5d140b9c3057
EraserHatchling <ismadaniel23sept@gmail.com>
2021-12-19 03:28:02 +0000

    commit1

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    let userName = "";
+    let userAge = "";
+async function getWeather() {
+  fetch('https://api.agify.io/?name=' + userName)
+	.then((response) => response.json())
+	.then((data) => {
+  	userName = data.name; // Path for temperature
+  	userAge = data.age; // Path for description
+	});
+}
+</script>
